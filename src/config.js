633a482a4e066c3ea267c3c2c88ffc9e646f78bd
Carlos Martinez <martinezcj2@gmail.com>
2018-08-05 23:48:13 -0400

    Create config file for aws configuration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-uploads-aws"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ur67km7jac.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_NKcl3ivUr",
+    APP_CLIENT_ID: "2m4hge1lccakltn0kah40l4256",
+    IDENTITY_POOL_ID: "us-east-2:7e9d081d-c775-4989-bd7e-ca18195bda48"
+  }
+};
