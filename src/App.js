04230afb7f884fe85ef0fac9cf439a3d4ff9da14
Carlos Martinez <martinezcj2@gmail.com>
2018-08-06 01:42:38 -0400

    Add authenticated state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ import Routes from './Routes';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
@@ -28,7 +45,7 @@ class App extends Component {
             </Nav>
           </Navbar.Collapse> 
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
