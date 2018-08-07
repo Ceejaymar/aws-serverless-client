ed384b862219ac62cbf180e6e4eba1ceca95735a
Carlos Martinez <martinezcj2@gmail.com>
2018-08-07 02:17:37 -0400

    Add appliedroute for signup

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from "./components/AppliedRoute";
 import Home from './containers/Home';
 import Login from './containers/Login';
+import SignUp from './containers/SignUp';
 import NotFound from './containers/NotFound';
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={SignUp} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
